0b1be808911a9f5687475a66154e05521a4cebcc
THIAGO <thiagorangeldasilva43@gmail.com>
2024-04-28 23:27:55 -0300

    estilizando-paginas

--- recuperaBancos.js
+++ recuperaBancos.js
@@ -5,7 +5,9 @@ const recuperarBancos = (res) => {
     let consulta = 'SHOW DATABASES'
     connection.query(consulta,(err,results)=>{
         if (err) throw err
-        const databases = results.map(result => result.Database)
+        const databases = results
+        .map(result => result.Database)
+        .filter(database => !['information_schema','mysql', 'performance_schema', 'sys','phpmyadmin','test'].includes(database))
         
         res.writeHead(200, {'Content-Type': 'application/json'})
         res.end(JSON.stringify(databases))
